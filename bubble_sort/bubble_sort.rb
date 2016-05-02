13b31eb2dada473d67fb79e70d32e94a444bba6b
Richard-Scap <scap222@gmail.com>
2016-05-02 18:01:32 -0500

    refactor bubblesort

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -1,23 +1,24 @@
-require 'pry'
-
 class BubbleSort
-
-
-
-	arr = [1,4,3,7,8,2,4,9,0,3]
-	swap = nil
-	n = arr.length
-	i = 0
-
-	while i < n do
-		arr.each_with_index do | num, index |
-			if index == 0
-				swap = false				
-			elsif arr[index-1] > arr[index]
-				arr[index-1], arr[index] = arr[index], arr[index-1]					
-			end
-		end
-		i = i+1
+	attr_accessor :arr
+	attr_accessor :i
+	
+	def initialize(arr)
+	#the array which will be sorted
+		@arr = arr 
+		@i = 0
 	end
-	print arr
+
+	def sort	
+		while i < arr.length do
+
+			arr.each_with_index do | num, index |
+				next if index == 0
+				if arr[index-1] > arr[index]
+					arr[index-1], arr[index] = arr[index], arr[index-1]					
+				end
+			end
+			@i += 1
+		end
+		return arr
+	end
 end
